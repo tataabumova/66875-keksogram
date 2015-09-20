4fcf137fce387b3d2c9978980a57941f38ee1213
Tata Abumova <tata.abumova@gmail.com>
2015-09-20 22:51:18 +0300

    Добавлена валидация формы resize Ограничения: смещение + размер итоговой фотографии не должны выходить за ее пределы.

--- js/resize-form.js
+++ js/resize-form.js
@@ -1,10 +1,33 @@
-(function() {
+//(function() {
   var uploadForm = document.forms['upload-select-image'];
   var resizeForm = document.forms['upload-resize'];
   var filterForm = document.forms['upload-filter'];
 
   var previewImage = resizeForm.querySelector('.resize-image-preview');
   var prevButton = resizeForm['resize-prev'];
+
+  var toLeft = resizeForm['resize-x'];
+  var toTop = resizeForm['resize-y'];
+  var sizePicture = resizeForm['resize-size'];
+
+  var MAX_SIZE = 582;
+  //var MAX_TO_TOP = 281;
+  //var MAX_TO_LEFT = 281;
+
+  toLeft.value = 291;
+  toTop.value = 291;
+  sizePicture.value = 291;
+
+  toLeft.onchange = function(evt) {
+    sizePicture.max = parseInt(MAX_SIZE - parseInt(toLeft.value, 10));
+  };
+  toTop.onchange = function(evt) {
+    sizePicture.max = parseInt(MAX_SIZE - parseInt(toTop.value, 10));
+  };
+  sizePicture.onchange = function(evt) {
+    toLeft.max = parseInt(MAX_SIZE - parseInt(sizePicture.value, 10));
+    toTop.max = parseInt(MAX_SIZE - parseInt(sizePicture.value, 10));
+  };
 
   prevButton.onclick = function(evt) {
     evt.preventDefault();
@@ -18,8 +41,10 @@
   resizeForm.onsubmit = function(evt) {
     evt.preventDefault();
     filterForm.elements['filter-image-src'] = previewImage.src;
+    //filterForm.elements['filter-image-src'] = previewImage.classList;
+
 
     resizeForm.classList.add('invisible');
     filterForm.classList.remove('invisible');
   };
-})();
+//})();
