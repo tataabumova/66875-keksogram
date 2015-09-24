720d7609209abd1312b78fbf9ce1e10d96b61753
tataabumova <169041989Zz>
2015-09-24 22:55:55 +0300

    add min and max default values for inputs

--- js/resize-form.js
+++ js/resize-form.js
@@ -14,15 +14,23 @@
   resizeForm['resize-y'].value = 0;
   resizeForm['resize-size'].value = 100;
 
+  resizeForm['resize-x'].min = 0;
+  resizeForm['resize-y'].min = 0;
+  resizeForm['resize-size'].min = 0;
+
   resizeForm['resize-x'].onchange = function(evt) {
     resizeForm['resize-size'].max = minSide() - parseInt(this.value, 10);
+    this.max = minSide();
+
   };
   resizeForm['resize-y'].onchange = function(evt) {
     resizeForm['resize-size'].max = minSide() - parseInt(this.value, 10);
+    this.max = minSide();
   };
   resizeForm['resize-size'].onchange = function(evt) {
     resizeForm['resize-x'].max = minSide() - parseInt(this.value, 10);
     resizeForm['resize-y'].max = minSide() - parseInt(this.value, 10);
+    this.max = minSide();
   };
 
   prevButton.onclick = function(evt) {
