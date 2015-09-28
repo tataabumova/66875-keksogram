43d3141f662f2b4a2c919ad82663310bc9bfe174
tataabumova <169041989Zz>
2015-09-29 00:21:31 +0300

    внесла правки по комментариям + добавила размеры картинкам

--- js/pictures.js
+++ js/pictures.js
@@ -1,47 +1,44 @@
 (function() {
-  document.querySelector('.filters').classList.add('hidden');
+  var filtersElement = document.querySelector('.filters');
+  filtersElement.classList.add('hidden');
 
   var picturesContainer = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture-template');
+  var pictureTemplateElement = pictureTemplate.content.children[0];
   var picturesFragment = document.createDocumentFragment();
 
   var REQUEST_FAILURE_TIMEOUT = 10000;
 
   pictures.forEach(function(picture, i) {
-    var newPictureElement = pictureTemplate.content.children[0].cloneNode(true);
+    var newPictureElement = pictureTemplateElement.cloneNode(true);
 
-    //newPictureElement.querySelector('.picture img').setAttribute('src', picture['url']);
-    newPictureElement.querySelector('.picture-comments').textContent = picture['comments'];
-    newPictureElement.querySelector('.picture-likes').textContent = picture['likes'];
+    newPictureElement.querySelector('.picture-comments').textContent = picture.comments;
+    newPictureElement.querySelector('.picture-likes').textContent = picture.likes;
 
     picturesFragment.appendChild(newPictureElement);
     picturesContainer.appendChild(picturesFragment);
 
-    if (picture['url']) {
+    if (picture.url) {
       var pictureImage = new Image();
-      pictureImage.src = picture['url'];
+      pictureImage.src = picture.url;
+      pictureImage.width = '182';
+      pictureImage.height = '182';
 
       var imageLoadTimeout = setTimeout(function () {
-        console.log('timeout');
         newPictureElement.classList.add('picture-load-failure');
       }, REQUEST_FAILURE_TIMEOUT);
 
       pictureImage.onload = function() {
-        console.log('onload');
-        //newPictureElement.style.backgroundImage = 'url(\'' + pictureImage.src + '\')';
-        //newPictureElement.style.backgroundSize = '182px 182px';
-        newPictureElement.querySelector('img').setAttribute('src', picture['url']);
-        var oldPicture = newPictureElement.querySelector('.picture img');
+        var oldPicture = newPictureElement.querySelector('img');
         newPictureElement.replaceChild(pictureImage, oldPicture);
         clearTimeout(imageLoadTimeout);
       };
 
       pictureImage.onerror = function() {
-        console.log('onerror');
         newPictureElement.classList.add('picture-load-failure');
       };
     }
-    document.querySelector('.filters').classList.remove('hidden');
+    filtersElement.classList.remove('hidden');
   });
 
 })();
