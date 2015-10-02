f131157af2e6fc96ce15c10bf8bc6e2d11dec4dd
tataabumova <169041989Zz>
2015-10-02 22:20:43 +0300

    Загрузка картинок по XMLHttpRequest.

--- js/pictures.js
+++ js/pictures.js
@@ -2,47 +2,102 @@
 
 
 (function() {
+  var ReadyState = {
+    'UNSENT': 0,
+    'OPENED': 1,
+    'HEADERS_RECEIVED': 2,
+    'LOADING': 3,
+    'DONE': 4
+  };
+
   var filtersElement = document.querySelector('.filters');
   filtersElement.classList.add('hidden');
-
-  var picturesContainer = document.querySelector('.pictures');
-  var pictureTemplate = document.querySelector('#picture-template');
-  var pictureTemplateElement = pictureTemplate.content.children[0];
-  var picturesFragment = document.createDocumentFragment();
-  var pictureSize = 182;
-
   var REQUEST_FAILURE_TIMEOUT = 10000;
+  var picturesContainer = document.querySelector('.pictures');
+  var pictures;
 
-  pictures.forEach(function(picture) {
-    var newPictureElement = pictureTemplateElement.cloneNode(true);
+  function renderPictures(picture) {
+    picturesContainer.classList.remove('picture-load-failure');
+    picturesContainer.innerHTML = '';
 
-    newPictureElement.querySelector('.picture-comments').textContent = picture.comments;
-    newPictureElement.querySelector('.picture-likes').textContent = picture.likes;
+    var pictureTemplate = document.querySelector('#picture-template');
+    var pictureTemplateElement = pictureTemplate.content.children[0];
+    var picturesFragment = document.createDocumentFragment();
+    var pictureSize = 182;
 
-    picturesFragment.appendChild(newPictureElement);
+    pictures.forEach(function(picture) {
+      var newPictureElement = pictureTemplateElement.cloneNode(true);
 
-    if (picture.url) {
-      var pictureImage = new Image();
-      pictureImage.src = picture.url;
-      pictureImage.width = pictureImage.height = pictureSize;
+      newPictureElement.querySelector('.picture-comments').textContent = picture.comments;
+      newPictureElement.querySelector('.picture-likes').textContent = picture.likes;
 
-      var imageLoadTimeout = setTimeout(function() {
-        newPictureElement.classList.add('picture-load-failure');
-      }, REQUEST_FAILURE_TIMEOUT);
+      picturesFragment.appendChild(newPictureElement);
 
-      pictureImage.onload = function() {
-        var oldPicture = newPictureElement.querySelector('img');
-        newPictureElement.replaceChild(pictureImage, oldPicture);
-        clearTimeout(imageLoadTimeout);
-      };
+      if (picture.url) {
+        var pictureImage = new Image();
+        pictureImage.src = picture.url;
+        pictureImage.width = pictureImage.height = pictureSize;
 
-      pictureImage.onerror = function() {
-        newPictureElement.classList.add('picture-load-failure');
-      };
-    }
-    filtersElement.classList.remove('hidden');
+        var imageLoadTimeout = setTimeout(function() {
+          newPictureElement.classList.add('picture-load-failure');
+        }, REQUEST_FAILURE_TIMEOUT);
+
+        pictureImage.onload = function() {
+          var oldPicture = newPictureElement.querySelector('img');
+          newPictureElement.replaceChild(pictureImage, oldPicture);
+          clearTimeout(imageLoadTimeout);
+        };
+
+        pictureImage.onerror = function() {
+          newPictureElement.classList.add('picture-load-failure');
+        };
+      }
+      filtersElement.classList.remove('hidden');
+    });
+    picturesContainer.appendChild(picturesFragment);
+  }
+
+  function showLoadFailure() {
+    picturesContainer.classList.add('pictures-failure');
+  }
+
+  function loadPictures(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.timeout = REQUEST_FAILURE_TIMEOUT;
+    xhr.open('get', 'data/pictures.json');
+    xhr.send();
+
+    xhr.onreadystatechange = function(evt) {
+      var loadedXhr = evt.target;
+
+      switch (loadedXhr.readyState) {
+        case ReadyState.OPENED:
+        case ReadyState.HEADERS_RECEIVED:
+        case ReadyState.LOADING:
+          picturesContainer.classList.add('pictures-loading');
+          break;
+
+        case ReadyState.DONE:
+        default:
+          if (loadedXhr.status === 200) {
+            var data = loadedXhr.response;
+            picturesContainer.classList.remove('pictures-loading');
+            callback(JSON.parse(data));
+          }
+
+          if (loadedXhr.status > 400) {
+            showLoadFailure();
+          }
+          break;
+      }
+    };
+
+    xhr.ontimeout = function() {
+      showLoadFailure();
+    };
+  }
+  loadPictures(function(loadedPictures) {
+    pictures = loadedPictures;
+    renderPictures(loadedPictures);
   });
-
-  picturesContainer.appendChild(picturesFragment);
-
 })();
