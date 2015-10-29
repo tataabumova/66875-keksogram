0418b69ebcc20bf1600c0f9c2ea617f63fccbb2e
Keks <keks@htmlacademy.ru>
2015-10-29 16:45:55 +0300

    :cat: Обновления от Кекса для модуля «Медиа и графика»

--- js/resize-form.js
+++ js/resize-form.js
@@ -1,4 +1,3 @@
-'use strict';
 (function() {
   var uploadForm = document.forms['upload-select-image'];
   var resizeForm = document.forms['upload-resize'];
@@ -6,33 +5,6 @@
 
   var previewImage = resizeForm.querySelector('.resize-image-preview');
   var prevButton = resizeForm['resize-prev'];
-
-  function minSide() {
-    return Math.min(previewImage.width, previewImage.height);
-  }
-
-  resizeForm['resize-x'].value = 0;
-  resizeForm['resize-y'].value = 0;
-  resizeForm['resize-size'].value = 100;
-
-  resizeForm['resize-x'].min = 0;
-  resizeForm['resize-y'].min = 0;
-  resizeForm['resize-size'].min = 0;
-
-  resizeForm['resize-x'].onchange = function() {
-    resizeForm['resize-size'].max = minSide() - parseInt(this.value, 10);
-    this.max = minSide();
-
-  };
-  resizeForm['resize-y'].onchange = function() {
-    resizeForm['resize-size'].max = minSide() - parseInt(this.value, 10);
-    this.max = minSide();
-  };
-  resizeForm['resize-size'].onchange = function() {
-    resizeForm['resize-x'].max = minSide() - parseInt(this.value, 10);
-    resizeForm['resize-y'].max = minSide() - parseInt(this.value, 10);
-    this.max = minSide();
-  };
 
   prevButton.onclick = function(evt) {
     evt.preventDefault();
